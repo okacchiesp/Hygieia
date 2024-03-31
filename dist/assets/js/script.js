ff1c06623af1f8a8e3e3ee369b7d38edc3da2f1f
Yuya Okano <Okacchi@Yuya-no-MacBook-Pro.local>
2024-03-31 21:48:54 +0900

    ドロワーメニュー

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -1,4 +1,15 @@
 "use strict";
 
-jQuery(function ($) {// この中であればWordpressでも「$」が使用可能になる
+jQuery(function ($) {
+  // この中であればWordpressでも「$」が使用可能になる
+  //ドロワーメニュー
+  $(".js-hamburger").click(function () {
+    if ($(".js-hamburger").hasClass("is-active")) {
+      $(this).removeClass("is-active");
+      $(".js-sp-nav").css("opacity", "0");
+    } else {
+      $(this).addClass("is-active");
+      $(".js-sp-nav").css("opacity", "1");
+    }
+  });
 });
--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,14 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
-
+  //ドロワーメニュー
+  $(".js-hamburger").click(function () {
+    if ($(".js-hamburger").hasClass("is-active")) {
+      $(this).removeClass("is-active");
+      $(".js-sp-nav").css("opacity", "0");
+    } else {
+      $(this).addClass("is-active");
+      $(".js-sp-nav").css("opacity", "1");
+    }
+  });
 
 });
